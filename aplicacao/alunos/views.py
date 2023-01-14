728c70e452f4e8e7852d69256169c14458ba29c9
Everton Denega Souza <everton.souza@proway.treina>
2023-01-13 21:38:29 -0300

    ok

--- aplicacao/alunos/views.py
+++ aplicacao/alunos/views.py
@@ -4,7 +4,19 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    alunos = {
+        1: 'Aluno 1',
+        2: 'Aluno 2',
+        3: 'Aluno 3',
+        4: 'Aluno 4',
+        5: 'Aluno 5',
+        6: 'Aluno 6',
+    }
+    dados = {
+        'nome_do_aluno' : alunos
+    }
+
+    return render(request,'index.html', dados)
 
 def aluno(request):
     return render(request,'aluno.html')
